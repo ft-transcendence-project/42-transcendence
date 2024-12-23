b6dded3eb1dd7b355a0bc345ffe766e14ee08e31
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-12-23 19:37:34 +0900

    [update] テストも修正

--- backend/oauth/tests.py
+++ backend/oauth/tests.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 from accounts.models import CustomUser
+from django.conf import settings
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -32,7 +33,10 @@ class OAuthCallbackViewTests(APITestCase):
 
         response = self.client.get(reverse("oauth:callback"), {"code": "test_code"})
         self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-        self.assertEqual(response.url, "http://localhost:3000/#/")
+        if settings.DEBUG == False:
+            self.assertEqual(response.url, "https://localhost:8443/#/")
+        else:
+            self.assertEqual(response.url, "http://localhost:3000/#/")
 
         user = CustomUser.objects.get(username="testuser")
         self.assertIsNotNone(user)
