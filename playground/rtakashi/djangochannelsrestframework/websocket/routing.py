148990cae36a89b45f17ce648c32d0d6e2af8ed3
retakashi <119090016+retakashi@users.noreply.github.com>
2024-12-23 12:48:39 +0900

    clean:routing.pyからGameStateComsumerへのパスを削除

--- playground/rtakashi/djangochannelsrestframework/websocket/routing.py
+++ playground/rtakashi/djangochannelsrestframework/websocket/routing.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .PongLogic import consumers
 
 websocket_urlpatterns = [
-    path("ws/gamelogic/", consumers.PongLogic.as_asgi()),
-    path("ws/gamestate/", consumers.GameStateConsumer.as_asgi()),
+    path("ws/gamelogic/", consumers.PongLogic.as_asgi())
 ]
