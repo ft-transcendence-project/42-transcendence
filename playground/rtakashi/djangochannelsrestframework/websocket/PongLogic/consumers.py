f9f578ac046ff2bbcfc93d16f3b2912905a7707c
retakashi <119090016+retakashi@users.noreply.github.com>
2024-12-23 12:43:24 +0900

    clean:GameStateConsumers削除

--- playground/rtakashi/djangochannelsrestframework/websocket/PongLogic/consumers.py
+++ playground/rtakashi/djangochannelsrestframework/websocket/PongLogic/consumers.py
@@ -5,91 +5,12 @@ import asyncio
 import math
 from .utils import Utils
 from .shared import SharedState
-from .serverside_pong_api import move_paddle, init_game, pause_game, resume_game, set_game_state
 from websocket.models import GameState
 from channels.db import database_sync_to_async
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
 from djangochannelsrestframework.consumers import AsyncAPIConsumer
 from websocket.serializers import GameStateSerializer
-from djangochannelsrestframework.mixins import (
-    ListModelMixin,
-    RetrieveModelMixin,
-    PatchModelMixin,
-    UpdateModelMixin,
-    CreateModelMixin,
-    DeleteModelMixin,
-)
 from asgiref.sync import sync_to_async
-from djangochannelsrestframework.decorators import action
-from djangochannelsrestframework.observer import model_observer
-
-class GameStateConsumer(SharedState,GenericAsyncAPIConsumer, ListModelMixin):
-    from websocket.models import GameState
-    queryset = GameState.objects.all()
-    serializer_class = GameStateSerializer
-    lookup_field = 'id' 
-    # ↑試合番号とか？
-    
-    async def receive_json(self, content, **kwargs):
-        action = content.get("action")
-        valid_actions = [
-            "move_up",
-            "move_down",
-            "init_game",
-            "pause_game",
-            "resume_game",
-            "get_game_state"
-        ]
-        if action not in valid_actions:
-            error_response = {
-                "error": f"Invalid action: {action}",
-                "valid_actions": valid_actions,
-            }
-            await self.send_json(error_response)
-            return
-        async with SharedState.lock:
-            if action == "get_game_state":
-                game_state = await database_sync_to_async(GameState.objects.last)()
-                if game_state:
-                # シリアライズして返す
-                    serialized_data = {
-                        "id": game_state.id,
-                        "state": game_state.state,
-                        "r_player": game_state.r_player,
-                        "l_player": game_state.l_player,
-                        "ball_x": game_state.ball_x,
-                        "ball_y": game_state.ball_y,
-                        "r_score": game_state.r_score,
-                        "l_score": game_state.l_score,
-                    }
-                #     serialized_data = self.get_serializer(game_state).data
-                    await self.send_json(serialized_data)
-                # else:
-                #     await self.send_json({"error": "No game state available"})
-                # return
-                    # await self.game_state_activity.subscribe()
-                    print(f"get_state:",game_state)
-                    return
-            if action == "move_up" or action == "move_down":
-                game_state = await move_paddle(action, content)
-            elif action == "init_game":
-                game_state = await init_game()
-            elif action == "pause_game":
-                game_state = await pause_game()
-            elif action == "resume_game":
-                game_state = await resume_game()
-            print(f"gamestate2",game_state)
-            response_message = Utils.create_game_update_message(SharedState.Ball, SharedState.Paddle, SharedState.Score)
-            await self.channel_layer.group_send(
-                "sendmessage",
-                {
-                    "type": "send_message",
-                    "content": response_message,
-                },
-            )
-            if action == "init_game":
-                await asyncio.sleep(2)
-            
 
 class PongLogic(SharedState, AsyncWebsocketConsumer):
     def __new__(cls, *args, **kwargs):
@@ -194,8 +115,7 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
             Utils.adjust_ball_position(
                 SharedState.Ball, SharedState.Paddle, velocity, SharedState.GameWindow
             )
-            await set_game_state("playing")
-
+            
     async def check_game_state(self):
         async with SharedState.lock:
             if (
@@ -207,8 +127,7 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
             elif SharedState.Ball.x + SharedState.Ball.radius < 0:
                 SharedState.Score.right += 1
                 self.state = "stop"
-            await set_game_state("playing")
-
+            
     async def connect(self):
         if "game_loop" in SharedState.tasks:
             SharedState.tasks["game_loop"].cancel()
@@ -217,13 +136,12 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
         await self.accept()
         self.state = "stop"
         SharedState.tasks["game_loop"] = asyncio.create_task(Utils.game_start(self.game_loop()))
-        await set_game_state("playing")
-    
+        
     async def disconnect(self, close_code):
         if "game_loop" in SharedState.tasks:
             SharedState.init()
             SharedState.tasks["game_loop"].cancel()
-            await set_game_state("waiting")
+            
         await self.channel_layer.group_discard("sendmessage", self.channel_name)
         print("Websocket disconnected")
 
@@ -251,7 +169,6 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
             elif key == "I" and action == "pressed":
                 if SharedState.Paddle.right_y - 3 >= 0:
                     SharedState.Paddle.right_y -= 3
-            await set_game_state("playing")
         if self.state == "stop":
             await self.send_pos()
 
@@ -268,6 +185,7 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
         )
 
     async def send_pos(self):
+        print("ball",SharedState.Ball.x)
         response_message = Utils.create_game_update_message(SharedState.Ball, SharedState.Paddle, SharedState.Score)
         await self.channel_layer.group_send(
             "sendmessage",
