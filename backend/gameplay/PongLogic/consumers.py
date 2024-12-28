a37653ed2c363e27ecb0f7f60c273f6bdf78980f
羅座謝瑠玖 <razashahrukh@ShahrukhMacBook-Air.local>
2024-12-28 10:51:01 +0100

    test:

--- backend/gameplay/PongLogic/consumers.py
+++ backend/gameplay/PongLogic/consumers.py
@@ -1,5 +1,5 @@
 import json
-
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 import asyncio
 # import random
@@ -16,17 +16,48 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
         super().__init__(*args, **kwargs)
         self.state = "stop"
         self.tasks = {}
-
-    # これを追加しました。raza send_posするときに引数これを呼んでください.
-    def get_winner(self):
-        if SharedState.Score.left > SharedState.Score.right:
-            return "left"
-        else:
-            return "right"
+        self.group_name = None
 
     # PongLogic
     async def game_loop(self):
         turn_count = 0
+        try:
+            from gameplay.models import GameSetting
+            setting = await sync_to_async(GameSetting.objects.get)(id=self.setting_id)
+            ball_size_choise = setting.ball_size
+            ball_v_choise = setting.ball_velocity
+            map_choise = setting.map
+            print(f"map: {map_choise}, ball_size: {ball_size_choise}, ball_v: {ball_v_choise}")
+            if ball_size_choise == "big":
+                SharedState.Ball.radius = 20
+            elif ball_size_choise == "normal":
+                SharedState.Ball.radius = 10
+            elif ball_size_choise == "small":
+                SharedState.Ball.radius = 5
+            if ball_v_choise == "fast":
+                SharedState.Ball.velocity = 7
+            elif ball_v_choise == "normal":
+                SharedState.Ball.velocity = 5
+            elif ball_v_choise == "slow":
+                SharedState.Ball.velocity = 3
+            if map_choise == "a":
+                SharedState.Obstacle.width = 0
+                SharedState.Obstacle.height = 0
+                SharedState.blind.width = 0
+                SharedState.blind.height = 0
+            elif map_choise == "b":
+                SharedState.Obstacle.width = 500
+                SharedState.Obstacle.height = 30
+                SharedState.blind.width = 0
+                SharedState.blind.height = 0
+            elif map_choise == "c":
+                SharedState.Obstacle.width = 0
+                SharedState.Obstacle.height = 0
+                SharedState.blind.width = 300
+                SharedState.blind.height = 600
+            print(f"map: {map_choise}, ball_size: {SharedState.Ball.radius}, ball_v: {SharedState.Ball.velocity}")
+        except Exception as e:
+            print(f"Error retrieving for GameSetting: {e}")
         while SharedState.Score.left < 15 and SharedState.Score.right < 15:
             async with SharedState.lock:
                 if self.state == "stop":
@@ -39,14 +70,13 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
                     Utils.set_direction(SharedState.Ball)
                 # print("angle: ", self.ball.angle)
                 # print("direction: ", self.ball.direction["facing_up"], self.ball.direction["facing_down"], self.ball.direction["facing_right"], self.ball.direction["facing_left"])
-            
             await self.rendering()
             await self.update_pos()
             await self.check_game_state()
-        await self.send_pos(self.get_winner())
+        await self.send_pos()
 
     async def rendering(self):
-        await self.send_pos(self.get_winner())
+        await self.send_pos()
         await asyncio.sleep(0.005)
         if self.state == "stop":
             await asyncio.sleep(2)
@@ -122,6 +152,9 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
             Utils.adjust_ball_position(
                 SharedState.Ball, SharedState.Paddle, velocity, SharedState.GameWindow
             )
+            Utils.update_obstacle_position(
+                SharedState.Obstacle, SharedState.GameWindow
+            )
 
     async def check_game_state(self):
         async with SharedState.lock:
@@ -131,15 +164,39 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
             ):
                 SharedState.Score.left += 1
                 self.state = "stop"
+                if SharedState.Score.left >= 15:
+                    await self.send_game_over("left")
             elif SharedState.Ball.x + SharedState.Ball.radius < 0:
                 SharedState.Score.right += 1
                 self.state = "stop"
+                if SharedState.Score.right >= 15:
+                    await self.send_game_over("right")
+
+    async def send_game_over(self, winner):
+        response_message = {
+            "type": "game_over",
+            "winner": winner,
+            "left_score": SharedState.Score.left,
+            "right_score": SharedState.Score.right
+        }
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": "send_message",
+                "content": response_message,
+            },
+        )
 
     async def connect(self):
+        self.setting_id = self.scope["url_route"]["kwargs"]["settingid"]
+        print(f"setting_id: {self.setting_id}")
+
+        self.group_name = f"game_{self.setting_id}"
+
         if "game_loop" in self.tasks:
             self.tasks["game_loop"].cancel()
-        await self.channel_layer.group_add("sendmessage", self.channel_name)
-        print("Websocket connected")
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        print(f"Websocket connected to group: {self.group_name}")
         await self.accept()
         self.tasks["game_loop"] = asyncio.create_task(self.game_loop())
 
@@ -147,8 +204,9 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
         if "game_loop" in self.tasks:
             SharedState.init()
             self.tasks["game_loop"].cancel()
-        await self.channel_layer.group_discard("sendmessage", self.channel_name)
-        print("Websocket disconnected")
+
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+        print(f"Websocket disconnected from group: {self.group_name}")
 
     async def receive(self, text_data=None):
         data = json.loads(text_data)
@@ -183,26 +241,31 @@ class PongLogic(SharedState, AsyncWebsocketConsumer):
         print(f"Other message received: {message}")
         response_message = {"message": f"Received: {message}"}
         await self.channel_layer.group_send(
-            "sendmessage",
+            self.group_name,
             {
                 "type": "send_message",
                 "content": response_message,
             },
         )
 
-    async def send_pos(self, winner=None):
+    async def send_pos(self):
         response_message = {
             "left_paddle_y": SharedState.Paddle.left_y,
             "right_paddle_y": SharedState.Paddle.right_y,
             "ball_x": SharedState.Ball.x,
             "ball_y": SharedState.Ball.y,
+            "ball_radius": SharedState.Ball.radius,
+            "obstacle_x": SharedState.Obstacle.x,
+            "obstacle_y": SharedState.Obstacle.y,
+            "obstacle_width": SharedState.Obstacle.width,
+            "obstacle_height": SharedState.Obstacle.height,
+            "blind_width": SharedState.blind.width,
+            "blind_height": SharedState.blind.height,
             "left_score": SharedState.Score.left,
             "right_score": SharedState.Score.right,
-            # これを追加しました。raza (どこかget_winnerを呼ぶのを忘れたとしてもここで追加できるようにしています。)
-            "winner": winner if SharedState.Score.left >= 15 or SharedState.Score.right >= 15 else None,
         }
         await self.channel_layer.group_send(
-            "sendmessage",
+            self.group_name,
             {
                 "type": "send_message",
                 "content": response_message,
