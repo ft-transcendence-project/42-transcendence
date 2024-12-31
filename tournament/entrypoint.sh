86f70db4896b1404db574ca29dc6b1eb0df3559c
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-12-31 22:11:41 +0900

    [update] tournament serviceにDocker用のファイルを追加

---
+++ tournament/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /usr/src/app/backend
+
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+# todo: 入れる(staticディレクトリを作る)
+# python manage.py collectstatic --noinput
+
+# 環境変数のENVIRONMENTの値がdevelopmentの時はrunserverを、productionの時はgunicornを実行
+if [ $ENVIRONMENT = "development" ]
+then
+    python manage.py runserver 0.0.0.0:8000
+else
+    gunicorn -b 0.0.0.0 -p 8000 core.wsgi:application
+fi
