d2adcf6aeb28e9b01c22e4356f255c8b7e3ee540
kkodaira <162079650+kkodaira@users.noreply.github.com>
2025-01-02 15:44:34 +0900

    web->postmanの順に切断されるとエラーが発生する問題を解決

--- gameplay/entrypoint.sh
+++ gameplay/entrypoint.sh
@@ -12,7 +12,7 @@ python manage.py migrate --noinput
 # todo: 入れる(staticディレクトリを作る)
 # python manage.py collectstatic --noinput
 
-python manage.py flush --noinput
+# python manage.py flush --noinput
 
 # 環境変数のENVIRONMENTの値がdevelopmentの時はrunserverを、productionの時はdaphneを実行
 if [ $ENVIRONMENT = "development" ]
