e4abe1fd4e3fe50336b78704dce79438b49faf48
eito2002 <126877725+eito2002@users.noreply.github.com>
2025-01-03 16:30:57 +0900

    [update] 各サービスでタグをLogstashに送信し、記録できるように

--- backend-services/tournament/core/settings.py
+++ backend-services/tournament/core/settings.py
@@ -155,7 +155,7 @@ LOGGING = {
             "version": 1,  # Version of logstash event schema. Default value: 0 (for backward compatibility of the library)
             "message_type": "django",  # 'type' field in logstash message. Default value: 'logstash'.
             "fqdn": False,  # Fully qualified domain name. Default value: false.
-            "tags": ["django"],  # list of tags. Default: None.
+            "tags": ["tournament"],  # list of tags. Default: None.
         },
     },
     "loggers": {
