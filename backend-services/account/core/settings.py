7c19d06641f1a5b15d41c585c182e7f9275e1221
eito2002 <126877725+eito2002@users.noreply.github.com>
2025-01-03 17:09:58 +0900

    [update] dbをサービスごとに分離してprodでのマイグレーションエラーを解消

--- backend-services/account/core/settings.py
+++ backend-services/account/core/settings.py
@@ -87,7 +87,7 @@ import os
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME"),
+        "NAME": os.environ.get("ACCOUNT_DB_NAME"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
--- backend-services/gameplay/core/settings.py
+++ backend-services/gameplay/core/settings.py
@@ -97,7 +97,7 @@ import os
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME"),
+        "NAME": os.environ.get("GAMEPLAY_DB_NAME"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
