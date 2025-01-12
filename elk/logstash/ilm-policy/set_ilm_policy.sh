8133877c1356ff97d5a79458aae570d7934f8709
eito2002 <126877725+eito2002@users.noreply.github.com>
2025-01-12 16:56:09 +0900

    [update] ilm-policyの名前更新

--- elk/logstash/ilm-policy/set_ilm_policy.sh
+++ elk/logstash/ilm-policy/set_ilm_policy.sh
@@ -5,8 +5,8 @@ KIBANA_USER="elastic"
 KIBANA_PASS=${KIBANA_PASSWORD}
 
 # ILMポリシーの適用
-curl -X PUT "${ELASTIC_HOST}/_ilm/policy/logs-policy" \
+curl -X PUT "${ELASTIC_HOST}/_ilm/policy/logstash-logs-policy" \
   -H "Content-Type: application/json" \
   --cacert /usr/share/logstash/config/certs/ca/ca.crt \
   -u ${KIBANA_USER}:${KIBANA_PASSWORD} \
-  -d @/usr/share/logstash/config/ilm-policy.json
+  -d @/usr/share/logstash/config/logstash-ilm-policy.json
