20d2236872762a2d1126164a9f27cb10d18a95d7
eito2002 <126877725+eito2002@users.noreply.github.com>
2025-01-12 16:27:21 +0900

    [update] Kibanaのindex_patternもjsonファイルを分離

--- elk/logstash/create_index_pattern.sh
+++ elk/logstash/create_index_pattern.sh
@@ -3,41 +3,10 @@
 KIBANA_HOST="https://kibana:5601"
 KIBANA_USER="elastic"
 KIBANA_PASS=${KIBANA_PASSWORD}
-INDEX_PATTERN_NAME="Logstash data view"
-INDEX_PATTERN_TITLE="logstash-*"
-
-cat <<EOF > index-pattern.json
-{
-  "data_view": {
-    "name": "${INDEX_PATTERN_NAME}",
-    "title": "${INDEX_PATTERN_TITLE}",
-    "timeFieldName": "@timestamp",
-    "fields": {
-      "tags": {
-        "name": "tags",
-        "type": "keyword",
-        "aggregatable": true
-      },
-      "message_type": {
-        "name": "message_type",
-        "type": "keyword",
-        "aggregatable": true
-      },
-      "level": {
-        "name": "level",
-        "type": "keyword",
-        "aggregatable": true
-      }
-    }
-  }
-}
-EOF
 
 curl -X POST "${KIBANA_HOST}/api/data_views/data_view" \
   --cacert /usr/share/logstash/config/certs/ca/ca.crt \
   -H "kbn-xsrf: string" \
   -H "Content-Type: application/json" \
   -u ${KIBANA_USER}:${KIBANA_PASS} \
-  --data @index-pattern.json
-
-rm index-pattern.json
+  --data @/usr/share/logstash/config/index-pattern.json
