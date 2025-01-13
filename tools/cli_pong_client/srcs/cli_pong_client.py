407494332b5646fce32f787e2989bb09b736e960
retakashi <119090016+retakashi@users.noreply.github.com>
2025-01-13 21:44:54 +0900

    fix:コンフリクト解消

--- tools/cli_pong_client/srcs/cli_pong_client.py
+++ tools/cli_pong_client/srcs/cli_pong_client.py
@@ -48,33 +48,8 @@ class PaddleControl:
             else:
                 Utils.print_colored_message("red", "Invalid input. Please type D(Left) or K(Right).")
         Utils.print_colored_message("yellow", "\nOK. You control \n\n\" ----- " + ("Left" if self.paddle_side == "left" else "Right") + " ----- \"\n")
-
+    
     def copy_certificate_from_docker(self) -> str:
-            if key == self.down_key:
-                self.send_delayed_message(ws, 'down', 'start', self.paddle_side)
-
-            if key == self.up_key:
-                self.send_delayed_message(ws, 'up', 'start', self.paddle_side)
-
-    def start(self, ws):
-        Utils.print_colored_message("green", "Start by typing the space key!")
-        while (True):
-            user_input = sys.stdin.readline().rstrip('\n')
-            if user_input == ' ':
-                message = {
-                    "game_signal": "start"
-                }
-                ws.send(json.dumps(message))
-                break
-
-        if self.paddle_side == 'left':
-            Utils.print_colored_message("white", "Controls: D - down, E - up, Q - Quit")
-            self.down_key = 'd'
-            self.up_key = 'e'
-        elif self.paddle_side == 'right':
-            Utils.print_colored_message("white", "Controls: K - down, I - up, Q - Quit")
-            self.down_key = 'k'
-            self.up_key = 'i'
         try:
             subprocess.run(["docker", "cp", "web:/etc/ssl/certs/cert.pem", "."], check=True,stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL)
@@ -99,7 +74,7 @@ class PaddleControl:
         )
         try:
             # wss接続し、WebSocketのイベントループを開始
-            ret = ws_app.run_forever(sslopt={"cert_reqs": ssl.CERT_REQUIRED,"ca_certs": cert_file_path})
+            ws_app.run_forever(sslopt={"cert_reqs": ssl.CERT_REQUIRED,"ca_certs": cert_file_path})
         except Exception as e:
             Utils.print_colored_message("red", f"Error: {e}")
             return
@@ -131,6 +106,16 @@ class PaddleControl:
         Utils.print_colored_message("red", "Disconnected\n")
 
     def start(self, ws):
+        Utils.print_colored_message("green", "Start by typing the space key!")
+        while (True):
+            user_input = sys.stdin.readline().rstrip('\n')
+            if user_input == ' ':
+                message = {
+                    "game_signal": "start"
+                }
+                ws.send(json.dumps(message))
+                break
+
         if self.paddle_side == 'left':
             Utils.print_colored_message("white", "Controls: D - down, E - up, Q - Quit")
             self.down_key = 'd'
@@ -143,7 +128,7 @@ class PaddleControl:
             self.handle_input(ws)
         except KeyboardInterrupt:
             self.running = False
-    
+
     def handle_input(self, ws):
         while self.running:
             key = self.get_key().lower()
@@ -183,7 +168,6 @@ class PaddleControl:
             "side": side
         }
         ws.send(json.dumps(message))
-
 
     def main(self):
         self.first_setup()
