fa0ca12edc9b562f96dce0ef9e2c5fd3ed7274a7
eito2002 <126877725+eito2002@users.noreply.github.com>
2025-01-13 16:56:04 +0900

    [update] index templateを作る部分もファイル分け

--- elk/logstash/template/create_template.sh
+++ elk/logstash/template/create_template.sh
@@ -4,9 +4,9 @@ ELASTIC_HOST="https://es01:9200"
 KIBANA_USER="elastic"
 KIBANA_PASS=${KIBANA_PASSWORD}
 
-# Create ILM policy
-curl -X PUT "${ELASTIC_HOST}/_ilm/policy/logstash-logs-policy" \
-  -H "Content-Type: application/json" \
+# Create index template
+curl -X PUT "${ELASTIC_HOST}/_template/logstash" \
   --cacert /usr/share/logstash/config/certs/ca/ca.crt \
+  -H "Content-Type: application/json" \
   -u ${KIBANA_USER}:${KIBANA_PASSWORD} \
-  -d @/usr/share/logstash/config/logstash-ilm-policy.json
+  -d @/usr/share/logstash/config/logstash-template.json
