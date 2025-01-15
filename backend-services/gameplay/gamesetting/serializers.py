bf5f8131d65bf93996394516eeffaf96d89524c4
kkodaira <162079650+kkodaira@users.noreply.github.com>
2025-01-15 19:58:48 +0900

    [delete] serializers.pyのコメント削除

--- backend-services/gameplay/gamesetting/serializers.py
+++ backend-services/gameplay/gamesetting/serializers.py
@@ -1,11 +1,6 @@
 from rest_framework import serializers
 from .models import GameSetting
 
-# class GameSettingSerializer(serializers.ModelSerializer):
-# 	class Meta:
-# 		model = GameSetting
-# 		fields = '__all__'
-
 class GameSettingSerializer(serializers.ModelSerializer):
     class Meta:
         model = GameSetting
