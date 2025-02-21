af8a3d3e32d5673efcf99288c82656ac1fbfc40a
retakashi <119090016+retakashi@users.noreply.github.com>
2025-02-21 13:25:58 +0900

    clean:Gameplay.jsの部品をクラスに

--- frontend/views/pages/Gameplay.js
+++ frontend/views/pages/Gameplay.js
@@ -1,7 +1,6 @@
 import { fetchWithHandling } from "../../utils/fetchWithHandling.js";
 import { fetchHtml } from "../../utils/fetchHtml.js";
-import { setUpGameCanvas } from "../../utils/game/gameCanvasConfig.js";
-import { getGameOptions } from "../../utils/game/gameCanvasConfig.js";
+import {Game, Paddle, Ball, Obstacle, Score} from "../../utils/game/Game.js";
 
 const Gameplay = {
   render: async () => {
@@ -22,39 +21,24 @@ const Gameplay = {
   },
 
   after_render: async () => {
-    const { gameCanvas, ctx, center_x, center_y , settingId} = setUpGameCanvas();
-    const { obstacle1, obstacle2, blind } = getGameOptions(center_x, center_y);
     let first = true;
-    let paddle_h = 120;
-    let paddle_w = 15;
-    let paddle = {
-      left_x: 0,
-      right_x: gameCanvas.width,
-      left_y: center_y - paddle_h / 2,
-      right_y: center_y - paddle_h / 2,
-    };
-    let ball = {
-      x: center_x,
-      y: center_y,
-      radius: 10,
-    };
-    let score = {
-      left: 0,
-      right: 0,
-    };
-
+    const game = new Game();
+    let paddle = new Paddle(game);
+    let score = new Score();
+    let obstacle1 = null;
+    let obstacle2 = null;
+    let blind = null;
+    let ball = null;
     let animationFrameId = null;
+    game.displayGameId();
+    game.displayPlayers();
     // Websocket
-    const url = `${window.env.GAMEPLAY_WS_HOST}/ponglogic/${settingId}/`;
+    const url = `${window.env.GAMEPLAY_WS_HOST}/ponglogic/${game.settingId}/`;
     window.ws = new WebSocket(url);
     console.log(url + " WebSocket created");
 
     window.ws.onopen = () => {
       console.log("WebSocket opened");
-      if (animationFrameId) {
-        cancelAnimationFrame(animationFrameId);
-      }
-      animationFrameId = requestAnimationFrame(update);
     };
 
     window.ws.onerror = (error) => {
@@ -249,33 +233,23 @@ const Gameplay = {
       }
 
       function updateGameState(data) {
-        score.left = data.left_score;
-          score.right = data.right_score;
-          paddle.left_y = data.left_paddle_y;
-          paddle.right_y = data.right_paddle_y;
-          ball.x = data.ball_x;
-          ball.y = data.ball_y;
-          if (first) {
-            ball.radius = data.ball_radius;
-            obstacle1.x = data.obstacle1_x;
-            obstacle1.y = data.obstacle1_y;
-            obstacle1.width = data.obstacle1_width;
-            obstacle1.height = data.obstacle1_height;
-            obstacle2.x = data.obstacle2_x;
-            obstacle2.y = data.obstacle2_y;
-            obstacle2.width = data.obstacle2_width;
-            obstacle2.height = data.obstacle2_height;
-            blind.x = data.blind_x;
-            blind.y = data.blind_y;
-            blind.width = data.blind_width;
-            blind.height = data.blind_height;
-            first = false;
-          }
-      }
-      } catch (error) {
+        if (first) {
+          obstacle1 = new Obstacle(data.obstacle1_x, data.obstacle1_y, data.obstacle1_width, data.obstacle1_height);
+          obstacle2 = new Obstacle(data.obstacle2_x, data.obstacle2_y, data.obstacle2_width, data.obstacle2_height);
+          blind = new Obstacle(data.blind_x, data.blind_y, data.blind_width, data.blind_height);
+          ball = new Ball(game, data.ball_radius);
+          first = false;
+          if (!animationFrameId)
+            animationFrameId = requestAnimationFrame(update);
+        }
+        score.setScore(data.left_score, data.right_score);
+        paddle.setPaddlePosition(data.left_paddle_y, data.right_paddle_y);
+        ball.setBallPosition(data.ball_x, data.ball_y);
+      } 
+    }catch (error) {
         console.error("WebSocket message error:", error);
-      }
-    };
+    }
+  };
 
     function sendMessage(message) {
       if (window.ws === null || window.ws.readyState !== WebSocket.OPEN) return;
@@ -380,39 +354,18 @@ const Gameplay = {
       window.keydownListenerAdded = true;
     }
 
-    
     // 描画関数
     function draw() {
-      ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-      ctx.fillStyle = "black";
-      ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-
-      ctx.fillStyle = "white";
-      ctx.fillRect(paddle.left_x, paddle.left_y, paddle_w, paddle_h);
-      ctx.fillRect(
-        paddle.right_x - paddle_w,
-        0 + paddle.right_y,
-        paddle_w,
-        paddle_h,
-      );
-      ctx.fillRect(
-        ball.x - ball.radius,
-        ball.y - ball.radius,
-        2 * ball.radius,
-        2 * ball.radius,
-      );
-
-      ctx.fillStyle = "red";
-      ctx.fillRect(blind.x, blind.y, blind.width, blind.height);
-
-      ctx.fillStyle = "yellow";
-      ctx.fillRect(obstacle1.x, obstacle1.y, obstacle1.width, obstacle1.height);
-      ctx.fillRect(obstacle2.x, obstacle2.y, obstacle2.width, obstacle2.height);
-
-      ctx.fillStyle = "white";
-      ctx.font = "50px Arial";
-      ctx.fillText(score.left, center_x - 50, 50);
-      ctx.fillText(score.right, center_x + 50, 50);
+      game.drawGameCanvas();
+      paddle.drawPaddle(game);
+      ball.drawBall(game);
+      score.drawScore(game);
+      if (obstacle1)
+        obstacle1.drawObstacle(game,"yellow");
+      if (obstacle2)
+        obstacle2.drawObstacle(game,"yellow");
+      if (blind)
+        blind.drawObstacle(game,"red");
     }
 
     let lastUpdateTime = 0;
@@ -424,8 +377,6 @@ const Gameplay = {
       }
       animationFrameId = requestAnimationFrame(update);
     }
-
-    update();
   },
 
   cleanup: async () => {
